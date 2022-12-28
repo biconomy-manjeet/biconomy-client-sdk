24b4bd890578af7406c8ae79156d613672a450a0
talha <talha.malik@ideofuzion.com>
2022-12-28 16:16:35 +0500

    unnecessary changes removed

---
+++ packages/common/src/Types/index.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SingletonFactory__factory = exports.SampleRecipient__factory = exports.SimpleWallet__factory = exports.IWallet__factory = exports.IStakeManager__factory = exports.IEntryPoint__factory = exports.IAggregator__factory = exports.BaseWallet__factory = void 0;
+var BaseWallet__factory_1 = require("./factories/BaseWallet__factory");
+Object.defineProperty(exports, "BaseWallet__factory", { enumerable: true, get: function () { return BaseWallet__factory_1.BaseWallet__factory; } });
+var IAggregator__factory_1 = require("./factories/IAggregator__factory");
+Object.defineProperty(exports, "IAggregator__factory", { enumerable: true, get: function () { return IAggregator__factory_1.IAggregator__factory; } });
+var IEntryPoint__factory_1 = require("./factories/IEntryPoint__factory");
+Object.defineProperty(exports, "IEntryPoint__factory", { enumerable: true, get: function () { return IEntryPoint__factory_1.IEntryPoint__factory; } });
+var IStakeManager__factory_1 = require("./factories/IStakeManager__factory");
+Object.defineProperty(exports, "IStakeManager__factory", { enumerable: true, get: function () { return IStakeManager__factory_1.IStakeManager__factory; } });
+var IWallet__factory_1 = require("./factories/IWallet__factory");
+Object.defineProperty(exports, "IWallet__factory", { enumerable: true, get: function () { return IWallet__factory_1.IWallet__factory; } });
+var SimpleWallet__factory_1 = require("./factories/SimpleWallet__factory");
+Object.defineProperty(exports, "SimpleWallet__factory", { enumerable: true, get: function () { return SimpleWallet__factory_1.SimpleWallet__factory; } });
+var SampleRecipient__factory_1 = require("./factories/SampleRecipient__factory");
+Object.defineProperty(exports, "SampleRecipient__factory", { enumerable: true, get: function () { return SampleRecipient__factory_1.SampleRecipient__factory; } });
+var SingletonFactory__factory_1 = require("./factories/SingletonFactory__factory");
+Object.defineProperty(exports, "SingletonFactory__factory", { enumerable: true, get: function () { return SingletonFactory__factory_1.SingletonFactory__factory; } });
+//# sourceMappingURL=index.js.map
